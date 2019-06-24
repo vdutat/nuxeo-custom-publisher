e2a80d56bebd1e79eb57832b9d14f3f852dc20e0
Vincent Dutat <vdutat@nuxeo.com>
2019-06-24 15:33:41 -0700

    save initial work

---
+++ nuxeo-custom-publisher-core/src/main/java/com/acme/publisher/CustomPublicationTree.java
@@ -0,0 +1,12 @@
+package com.acme.publisher;
+
+import org.nuxeo.ecm.platform.publisher.impl.core.SectionPublicationTree;
+
+public class CustomPublicationTree extends SectionPublicationTree {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
